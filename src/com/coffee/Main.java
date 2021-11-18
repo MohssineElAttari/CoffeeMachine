ed10e999a11c71c26e3e322726fede0fea7e019f
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-11-18 23:13:47 +0100

    add function check Choix

--- src/com/coffee/Main.java
+++ src/com/coffee/Main.java
@@ -4,13 +4,9 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
-		int choix;
-		// menu
-		System.out.println("--Binvenue chez Coffee Youcode--\n");
+	private static Scanner input;
 
+	public static void Menu() {
 		System.out.println("\t****** Menu Coffee ****** \n");
 		System.out.println("1 : Americano \t\t\t 3DH");
 		System.out.println("2 : Cappuccino \t\t\t 5DH");
@@ -22,14 +18,61 @@ public class Main {
 
 		System.out.println("\t****** Menu Lait ****** \n");
 		System.out.println("6 :  Lait chaud \t\t 1DH\n");
-		
+
 		System.out.println("\t****** Menu Tea ****** \n");
 		System.out.println("7 :  Tea limon \t\t\t 2DH\n");
-		
+
 		System.out.println("8 : Quitter");
-		
-		System.out.println("Entrer votre choix : ");
-		choix = input.nextInt();
+	}
+
+	public static void checkChoix(int choix) {
+		switch (choix) {
+		case 1:
+
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		case 5:
+
+			break;
+		case 6:
+
+			break;
+		case 7:
+
+			break;
+		case 8:
+
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		input = new Scanner(System.in);
+		int choix;
+		// menu
+
+		System.out.println("--Binvenue chez Coffee Youcode--\n");
+		do {
+			Menu();
+			System.out.println("Entrer votre choix :");
+			choix = input.nextInt();
+			if (choix < 1 || choix > 8) {
+				System.out.println("--Votre choix est invalid ! --\n");
+			}
+		} while (choix < 1 || choix > 8);
+		checkChoix(choix);
 
 	}
 
