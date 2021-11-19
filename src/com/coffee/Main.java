2b5dba7655273a6de0a0977facafb3f3b790c5f7
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-11-19 21:49:46 +0100

    update package

--- src/com/coffee/Main.java
+++ src/com/coffee/Main.java
@@ -6,6 +6,18 @@ public class Main {
 
 	private static Scanner input;
 
+	private static int prixEntree = 0;
+	private static int pieceEntrer = 0;
+
+	private static int dirham = 1;
+	private static int quntitieDirham = 10;
+
+	private static int deuxDirham = 2;
+	private static int quntitiedeuxDirham = 10;
+
+	private static int cinqDirham = 5;
+	private static int quntitiecinqDirham = 10;
+
 	public static void Menu() {
 		System.out.println("\t****** Menu Coffee ****** \n");
 		System.out.println("1 : Americano \t\t\t 3DH");
@@ -25,31 +37,101 @@ public class Main {
 		System.out.println("8 : Quitter");
 	}
 
+	public static int checkPiece(int piece, int prix) {
+		prixEntree = piece;
+		int newPrix;
+		if (prixEntree < 1) {
+			System.out.println("Le prix n'est pas valable");
+		}
+		while (prixEntree < prix) {
+			System.out.println("Le prix ne suffit pas");
+			System.out.println("Entrer un nouveux piece : ");
+			newPrix = input.nextInt();
+			prixEntree += newPrix;
+		}
+
+		return prixEntree;
+	}
+//operationCoffee
+	public static String operationCoffee(int piece, int prix) {
+		String resultat = "Argent restant : ";
+
+		prixEntree = piece - prix;
+		int nombrePiece = 0, restPiece = 0;
+		// System.out.println(prixEntree);
+		if (prixEntree > cinqDirham) {
+			if (quntitiecinqDirham > 1) {
+				nombrePiece = prixEntree / 5;
+				restPiece = prixEntree % 5;
+				if (restPiece>deuxDirham) {
+					
+				}
+				// System.out.println(restPiece);
+				quntitiecinqDirham = quntitiecinqDirham - 1;
+				resultat = resultat + nombrePiece+" (5DH) ";
+				
+			}
+		} else if (prixEntree < cinqDirham && prixEntree > deuxDirham) {
+			if (quntitiedeuxDirham > 1) {
+				prixEntree = prixEntree - 2;
+				quntitiedeuxDirham = quntitiedeuxDirham - 1;
+				resultat = resultat + "2DH ";
+			}
+		} else if (prixEntree < deuxDirham && prixEntree > dirham) {
+			if (quntitieDirham > 1) {
+				prixEntree = prixEntree - 1;
+				quntitieDirham = quntitieDirham - 1;
+				resultat = resultat + "1DH ";
+			}
+		}
+		return resultat;
+
+	}
+
 	public static void checkChoix(int choix) {
 		switch (choix) {
 		case 1:
-
+			System.out.println("Entrer un piece : ");
+			pieceEntrer = input.nextInt();
+			prixEntree = checkPiece(pieceEntrer, 3);
+			System.out.println(prixEntree);
+			System.out.println(operationCoffee(pieceEntrer, 3));
 			break;
 		case 2:
-
+			System.out.println("Entrer un piece : ");
+			pieceEntrer = input.nextInt();
+			prixEntree = checkPiece(pieceEntrer, 5);
+			System.out.println(operationCoffee(pieceEntrer, 5));
 			break;
 		case 3:
-
+			System.out.println("Entrer un piece : ");
+			pieceEntrer = input.nextInt();
+			prixEntree = checkPiece(pieceEntrer, 4);
+			System.out.println(operationCoffee(pieceEntrer, 4));
 			break;
 		case 4:
-
+			System.out.println("Entrer un piece : ");
+			pieceEntrer = input.nextInt();
+			prixEntree = checkPiece(pieceEntrer, 2);
+			System.out.println(operationCoffee(pieceEntrer, 2));
 			break;
 		case 5:
-
+			System.out.println("Entrer un piece : ");
+			pieceEntrer = input.nextInt();
+			prixEntree = checkPiece(pieceEntrer, 3);
+			System.out.println(operationCoffee(pieceEntrer, 3));
 			break;
 		case 6:
-
+			System.out.println("Entrer un piece : ");
+			pieceEntrer = input.nextInt();
+			prixEntree = checkPiece(pieceEntrer, 1);
+			System.out.println(operationCoffee(pieceEntrer, 1));
 			break;
 		case 7:
-
-			break;
-		case 8:
-
+			System.out.println("Entrer un piece : ");
+			pieceEntrer = input.nextInt();
+			prixEntree = checkPiece(pieceEntrer, 2);
+			System.out.println(operationCoffee(pieceEntrer, 2));
 			break;
 
 		default:
